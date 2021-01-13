a59682c2d2be0342d6eda74df8faeb66e3288892
Raphaël M-P <rmarquispelletier@gmail.com>
2021-01-13 14:53:28 -0500

    Modifier le state selon le texte

--- src/App.js
+++ src/App.js
@@ -3,38 +3,59 @@ import './App.css';
 import Character from "./Character"
 
 class App extends Component {
+
   constructor(props) {
-        super(props);
-        this.textToAnimate = "Puzzle's".split("");
-        this.counter = 0
-        this.colors = ['red', 'green', 'blue']
-        this.onClick = this.onClick.bind(this);
+    super(props);
+
+    const colors = ['red', 'green', 'blue'];
+    this.textToAnimate = "Puzzle's".split("");
+    this.counter = 0
+    this.colors = ['red', 'green', 'blue']
 
 
-        this.returnTest = function(e){
-          let temp = {}
-          temp.letter = e
-          temp.color = this.colors[Math.floor(Math.random() * this.colors.length)];
-          temp.id = this.counter;
-          this.counter++;
-          return temp
-        }
+    this.onClick = this.onClick.bind(this);
+    this.changeText = this.changeText.bind(this);
 
-        this.state = {
-          textString: "Puzzle's",
-//          textArray: this.state.textString.split(""),
-          time: 0,
-          textMap: [],
-        }
 
-        this.textToAnimate.forEach(element => {
-          this.state.textMap.push(this.returnTest(element))
-        }
-        );
+    this.returnTest = function(e){
+      let temp = {}
+      temp.letter = e
+      temp.color = colors[Math.floor(Math.random() * colors.length)];
+      temp.id = this.counter;
+      this.counter++;
+      return temp
     }
+
+    this.state = {
+      textString: "Puzzle's",
+//          textArray: this.state.textString.split(""),
+      time: 0,
+      textMap: [],
+    }
+
+    this.state.textString.split("").forEach(element => {
+      this.state.textMap.push(this.returnTest(element))
+    }
+    );
+  }
 
   onClick(e) {
     console.log('clicked')
+    this.setState(prevState => {
+          let randomColor = this.colors[Math.floor(Math.random() * this.colors.length)] //item gets random color
+          const updatedCharacters = prevState.textMap.map(item => {
+               item.color = randomColor
+               return item
+           })
+           return {
+               textMap: updatedCharacters
+           }
+       })
+  }
+
+  changeText(event) {
+    this.setState({textString: event.target.value});
+    console.log(event.target.value)
   }
 
   render() {
@@ -50,6 +71,7 @@ class App extends Component {
 
         </div>
         <button onClick={this.onClick}>Run</button>
+        <input onChange={this.changeText} value={this.state.textString}/>
       </div>
     );
   }
